b8f61965dab089ea85b170b3c6faef237cf16952
Chrysaliah <Chrysaliah@outlook.com>
2026-08-20 03:49:47 +0800

    fix: use query params for title to avoid header encoding issue

--- .github/scripts/send-push.js
+++ .github/scripts/send-push.js
@@ -2,33 +2,31 @@
 // 通过 GitHub Actions cron 触发，向手机弹系统通知
 const https = require('https');
 
-const TOPIC = 'ruozhao-reminders-2026';
+const TOPIC = process.env.NTFY_TOPIC || 'ruozhao-reminders-2026';
 const HOUR = process.env.PUSH_HOUR || '0';
 
 let title, body;
 if (HOUR === '8') {
   title = '学习计划时间到了';
-  body = '8:00 学习计划该推了，去拾光看看今天的安排\n\nhttps://chrysaliah.github.io/zhao-ming/';
+  body = '8:00 学习计划该推了，去拾光看看今天的安排';
 } else if (HOUR === '22') {
   title = '该写今日复盘了';
-  body = '22:30 学习复盘时间，去拾光写今天的复盘\n\nhttps://chrysaliah.github.io/zhao-ming/';
+  body = '22:30 学习复盘时间，去拾光写今天的复盘';
 } else {
   title = '拾光提醒';
-  body = '去看看今天的打卡和计划\n\nhttps://chrysaliah.github.io/zhao-ming/';
+  body = '去看看今天的打卡和计划';
 }
 
-const postData = body;
+const clickUrl = 'https://chrysaliah.github.io/zhao-ming/';
+const query = `?title=${encodeURIComponent(title)}&click=${encodeURIComponent(clickUrl)}&tags=bell`;
+const path = `/${TOPIC}${query}`;
 
 const options = {
   hostname: 'ntfy.sh',
-  path: `/${TOPIC}`,
+  path: path,
   method: 'POST',
   headers: {
-    'Content-Type': 'text/plain; charset=utf-8',
-    'Title': title,
-    'Tags': 'bell',
-    'Click': 'https://chrysaliah.github.io/zhao-ming/',
-    'Content-Length': Buffer.byteLength(postData)
+    'Content-Type': 'text/plain; charset=utf-8'
   }
 };
 
@@ -39,7 +37,7 @@ const req = https.request(options, (res) => {
     console.log('Status: ' + res.statusCode);
     console.log('Response: ' + data);
     if (res.statusCode === 200 || res.statusCode === 202) {
-      console.log('ntfy推送成功');
+      console.log('ntfy推送成功: ' + title);
     } else {
       console.log('推送失败');
       process.exit(1);
@@ -52,5 +50,5 @@ req.on('error', (e) => {
   process.exit(1);
 });
 
-req.write(postData);
+req.write(body);
 req.end();
